06809ac281fe0e046ee3fc153c0ce8b6ca8fdcf5
ellun <eltoncheng86@gmail.com>
2017-05-17 16:39:45 -0400

    Created ckan 2.x extension and plugin to update template

--- ckanext-ckantwo_theme/ckanext/ckantwo_theme/plugin.py
+++ ckanext-ckantwo_theme/ckanext/ckantwo_theme/plugin.py
@@ -1,39 +1,12 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-
-def most_popular_groups():
-    '''Return a sorted list of the groups with the most datasets.'''
-
-    # Get a list of all the site's groups from CKAN, sorted by number of
-    # datasets.
-    groups = toolkit.get_action('group_list')(
-        data_dict={'sort': 'package_count desc', 'all_fields': True})
-
-    # Truncate the list to the 10 most popular groups only.
-    groups = groups[:10]
-
-    return groups
-
 class Ckantwo_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
     # IConfigurer
 
-    # Declare that this plugin will implement ITemplateHelpers.
-    plugins.implements(plugins.ITemplateHelpers)
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'ckantwo_theme')
-
-    def get_helpers(self):
-        '''Register the most_popular_groups() function above as a template
-        helper function.
-        '''
-
-        # Template helper function names should begin with the name of the
-        # extension they belong to, to avoid clashing with functions from
-        # other extensions.
-        return {'ckantwo_theme_most_popular_groups': most_popular_groups}
