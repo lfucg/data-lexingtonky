c73b7f1ac6098a209bd91a2a055f0d1379ae112c
ellun <eltoncheng86@gmail.com>
2017-05-19 15:12:06 -0400

    Updating ruby version

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git submodule update --init --recursive
 virtualenv --python=/usr/bin/python3 ~/lfucg-deploy
 source ~/lfucg-deploy/bin/activate
 pip install boto3
-rvm use 2.2.2
+
 gem install berkshelf -v 5.6.3 --no-ri --no-rdoc
 
 ./deploy.py "$@"
